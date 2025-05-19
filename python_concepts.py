e8a49aae8e5dfd19ec75ee546bf71421093bd973
Ramunas <rojus@msn.com>
2025-05-19 14:37:38 +0300

    python_concepts.py

---
+++ python_concepts.py
@@ -0,0 +1,11 @@
+person_information_object = {
+    "name": "Ramunas",
+    "age": "52",
+    "city": "Kaunas"
+    
+}
+
+person_information_object["age"] = 53
+
+print("person_information_object:", person_information_object)
+print("person name:", person_information_object["name"])
