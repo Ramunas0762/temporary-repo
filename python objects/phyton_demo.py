bcb14a141f769df21b2603877f7e53a67910b490
Ramunas <rojus@msn.com>
2025-05-21 18:47:36 +0300

    python_demo.py

---
+++ python objects/phyton_demo.py
@@ -0,0 +1,35 @@
+import os
+from openai import OpenAI
+
+from dotenv import load_dotenv
+
+load_dotenv()  
+
+token = os.getenv("SECRET")
+endpoint = "https://models.github.ai/inference"
+model = "openai/gpt-4.1-nano"
+
+client = OpenAI(
+    base_url=endpoint,
+    api_key=token,
+)
+
+response = client.chat.completions.create(
+    messages=[
+        {
+            "role": "system",
+            "content": "You are a helpful assistant.",
+        },
+        {
+            "role": "user",
+            "content": "Tell me a joke about Lithuania. Always respond in Lithuanian.",
+        }
+    ],
+    temperature=1.0,
+    top_p=1.0,
+    model=model
+)
+
+print(response.choices[0].message.content)
+
+
